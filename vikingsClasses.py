768e9f9aaf0e2c838932c0fa9233db3449b7390d
Miquel de Toledo Mestres <pite2604@gmail.com>
2025-09-20 11:39:35 +0200

    We start the coding of the saxon's vs vikings war

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -4,57 +4,121 @@ import random
 
 
 class Soldier:
+
+    ## Define the properties on the constructor
+    
     def __init__(self, health, strength):
-        # your code here
+        self.health = health
+        self.strength = strength
+    
+    ## Return the strenght of the soldier
     
     def attack(self):
-        # your code here
-
+        return self.strength
+        
+    ## We don't need to return anything, we just susbtract the damage from the health
+    
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+
     
 
 # Viking
 
 class Viking(Soldier):
+    
+    ## We define viking's properties as the constructor
+    
     def __init__(self, name, health, strength):
-        # your code here
+        self.name = name
+        self.health = health
+        self.strength = strength
 
+    ## Define battlecry, don't take any arguments and just return a string
+    
     def battleCry(self):
-        # your code here
+        return "Odin Owns You All!"
 
     def receiveDamage(self, damage):
-        # your code here
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+            
 
 # Saxon
 
 class Saxon(Soldier):
+    
+    ## We define saxon's constructor, saxons has no names
+    
     def __init__(self, health, strength):
-        # your code here
-
+        self.health = health
+        self.strength = strength
+        
+    ## We define saxon's properties
+    
     def receiveDamage(self, damage):
-        # your code here
-
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return "A Saxon has died in combat"
+        
 # Davicente
 
 class War():
+    
+    ## The war constructor sholdn't recieve any arguments, we create the army of both sides
+    
     def __init__(self):
-        # your code here
+       self.VikingArmy = []
+       self.SaxonArmy = []
+
+    ## We append the new viking to the Viking Army 
 
     def addViking(self, viking):
-        # your code here
+        self.VikingArmy.append(viking)
+
+    ## We append the new saxon to the Saxon Army
     
     def addSaxon(self, saxon):
-        # your code here
+        self.SaxonArmy.append(saxon)
+    
+    ## We return the saxon's missing health from the vikings attack
     
     def vikingAttack(self):
-        # your code here
+        viking = random.randrange(len(self.VikingArmy))
+        saxon = random.randrange(len(self.SaxonArmy))
+        
+        result1 = saxon.self.recieveDamage(self, viking.strength)
+        
+        if saxon.health <= 0:
+            self.SaxonArmy.delete(saxon)
+        
+        return result1
+        
+    ## We return viking's missing health fromn the saxon's attack
     
     def saxonAttack(self):
-        # your code here
+        viking = random.randrange(len(self.VikingArmy))
+        saxon = random.randrange(len(self.SaxonArmy))
+        
+        result2 = viking.self.recieveDamage(self, saxon.strength)
+        
+        if viking.health <= 0:
+            self.VikingArmy.delete(viking)
+        
+        return result2
+    
 
     def showStatus(self):
-        # your code here
-    pass
-
+        if len(self.VikingArmy) == 0:
+            return f"Saxons have fought for their lives and survive another day..."
+        
+        elif len(self.SaxonArmy) == 0:
+            return f"Vikings have won the war of the century!"
+            
+        elif len(self.VikingArmy) >= 1 and len(self.SaxonArmy) >= 1:
+            return "Vikings and Saxons are still in the thick of battle."
 
