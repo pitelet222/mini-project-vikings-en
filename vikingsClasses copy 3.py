2a3aecd28e0c58fa6244eba7fd7d0d9376cf2e19
Miquel de Toledo Mestres <pite2604@gmail.com>
2025-09-24 17:00:26 +0200

    Add vikingsClasses copy 3.py with content from vikingsClasses copy 2.py

---
+++ vikingsClasses copy 3.py
@@ -0,0 +1,12 @@
+# Content of vikingsClasses copy 2.py
+
+# Assuming this is the content of the original file
+class Viking:
+    def __init__(self, name, strength):
+        self.name = name
+        self.strength = strength
+
+    def attack(self):
+        return f'{self.name} attacks with strength {self.strength}!'
+
+# More classes and functions can be added here...
